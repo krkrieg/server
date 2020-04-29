bf644e4a67b591aabcd4013cb6f84cc20f9581fa
Kristina Kriegert <kristina.kriegert2000@gmail.com>
2020-04-29 08:23:52 +0200

    Titel welcome changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>< FiLo System </title>
+        <title>FiLo System</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Styles -->
